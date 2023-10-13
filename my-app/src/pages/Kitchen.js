87ac353fb82b34ac8193c3ceba645dbe7b764ebc
la-rika <laramisiaforlani@gmail.com>
2023-10-13 17:59:39 +0200

    removed modal snippey

--- my-app/src/pages/Kitchen.js
+++ my-app/src/pages/Kitchen.js
@@ -138,15 +138,6 @@ export const Kitchen = () => {
                     )}
                 </div>
             </div>
-            {/* snippet della modale, da aggiungere anche js da bootstrap */}
-            {/* <div class="modal-body">
-                <h2 class="fs-5">Popover in a modal</h2>
-                <p>This <button class="btn btn-secondary" data-bs-toggle="popover" title="Popover title" data-bs-content="Popover body content is set in this attribute.">button</button> triggers a popover on click.</p>
-                <hr>
-                    <h2 class="fs-5">Tooltips in a modal</h2>
-                    <p><a href="#" data-bs-toggle="tooltip" title="Tooltip">This link</a> and <a href="#" data-bs-toggle="tooltip" title="Tooltip">that link</a> have tooltips on hover.</p>
-            </div> */}
-
         </div>
 
     )
