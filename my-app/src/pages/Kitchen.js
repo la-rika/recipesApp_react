68e7971232d52509f08bcd6161ac9e84848ffb58
la-rika <laramisiaforlani@gmail.com>
2023-10-22 22:31:16 +0200

    created logic for filter generate

--- my-app/src/pages/Kitchen.js
+++ my-app/src/pages/Kitchen.js
@@ -10,7 +10,11 @@ export const Kitchen = () => {
     const [sideDish, setSideDish] = useState('')
     const [activeStep, setActiveStep] = useState(1);
     const [allFood, setAllFood] = useState([])
-    let allf = []
+    const [mainFood, setMainFood] = useState([]);
+    const [secondFood, setSecondFood] = useState([]);
+    const [sideFood, setSideFood] = useState([]);
+    var allf = [];
+    var mf = [];
 
     //post request with axios
     const axiosAddData = (data) => {
@@ -23,20 +27,44 @@ export const Kitchen = () => {
     useEffect(() => {
         images.forEach((item, index) => {
             if (index !== 0) {
-                allf.push(item.name)
+                allf.push(item);
+                if (item.src.includes('pasta-rice') || item.src.includes('veggie') && item.name !== 'lettuce' && item.name !== 'corn') {
+                    mf.push(item)
+                    console.log()
+                }
             }
         })
         setAllFood(allf)
+        setMainFood(mf)
     }, [])
 
-    const onGenerate = (currentCourse) => {
+    const onGenerate = (currentCourse, currentFood) => { //TODO: ora maincourse e' un array di oggetti, sistemare il map per vedere i cibi nel form
         if (currentCourse.length < 3) {
-            //fare un find di un cibo che contiene le parole chiave per ogni portata e fare un random solo su quelli che corrispondono
-            let course = allFood[Math.floor(Math.random() * allFood.length)]
-
+            let course = currentFood[Math.floor(Math.random() * currentFood.length)];
+            let mc= mainCourse||[];
             switch (currentCourse) {
                 case mainCourse: {
-                    setMainCourse([...mainCourse, course]);
+                    console.log(course);
+                    if(mainCourse.find(item=>item.src.includes('pasta-rice'))){
+                        if(course.src.includes('pasta-rice')){
+                            onGenerate(mainCourse,mainFood)
+                        }else{
+                            if(!mainCourse.find(item=>item === course)){
+                                mc.push(course)
+
+                            }else{
+                                onGenerate(mainCourse,mainFood)
+                            }
+                        }
+                    }else{
+                        if(course.src.includes('pasta-rice')){
+                            mc.push(course)
+                        }else{
+                            onGenerate(mainCourse,mainFood)
+                        }
+                    }
+                    setMainCourse(mc)
+                    console.log(mainCourse)
                     break;
                 }
                 case secondCourse: {
@@ -110,12 +138,12 @@ export const Kitchen = () => {
             <div className="formDiv">
                 <form className="addFoodForm" action="/kitchen" method="post">
                     <img className="settingsImgKitchen" src={settingsImg} alt="filters-img" width="35px" height="35px" />
-                    <p><span style={{ fontWeight: 'bold' }}>main course (max 3):</span>{mainCourse.map((item) => ' ' + item + ', ')} </p>
+                    <p><span style={{ fontWeight: 'bold' }}>main course (max 3):</span>{mainCourse.map((item) => ' ' + item.name + ', ')} </p>
                     <p><span style={{ fontWeight: 'bold' }}>second course (max 3):</span> {secondCourse.map((item) => ' ' + item + ', ')} </p>
                     <p><span style={{ fontWeight: 'bold' }}>side dish (max 1):</span> {sideDish} </p>
                     <div className="formBtn">
                         <button type="button" className="btn"
-                            onClick={() => { onGenerate(activeStep === 1 ? mainCourse : activeStep === 2 ? secondCourse : sideDish) }}
+                            onClick={() => { if (activeStep === 1) { onGenerate(mainCourse, mainFood) } }}
                             disabled={mainCourse.length === 3 && activeStep === 1 ||
                                 secondCourse.length === 3 && activeStep === 2 ||
                                 sideDish !== '' && activeStep === 3}>GENERATE</button>
@@ -141,16 +169,16 @@ export const Kitchen = () => {
                 <div className="modal-dialog modal-lg">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="staticBackdropLabel" style={{marginLeft:'15px'}}>Recipe successfully saved !</h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" style={{width:'0px', height:'0px',marginRight:'15px'}} 
-                            onClick={()=>window.location.reload()}></button>
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel" style={{ marginLeft: '15px' }}>Recipe successfully saved !</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" style={{ width: '0px', height: '0px', marginRight: '15px' }}
+                                onClick={() => window.location.reload()}></button>
                         </div>
                         <div className="modal-body">
                             Click <b>STAY</b> to create another recipe or <b>LEAVE</b> to see all the recipes that you created.
                         </div>
                         <div className="modal-footer">
-                            <button  type="button" className="btn modal-button" data-bs-dismiss="modal" onClick={()=>window.location.reload()}>STAY</button>
-                            <button  type="button" className="btn modal-button" onClick={()=> window.location.replace('/my-recipes')}>LEAVE</button>
+                            <button type="button" className="btn modal-button" data-bs-dismiss="modal" onClick={() => window.location.reload()}>STAY</button>
+                            <button type="button" className="btn modal-button" onClick={() => window.location.replace('/my-recipes')}>LEAVE</button>
                         </div>
                     </div>
                 </div>
