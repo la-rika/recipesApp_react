ecd712afe3c7411d9e89e6ebbd5cb5f5ad3dd8c2
la-rika <laramisiaforlani@gmail.com>
2023-10-25 22:42:15 +0200

    added bin icon on cards

--- my-app/src/pages/MyRecipes.js
+++ my-app/src/pages/MyRecipes.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
+import images from "../indexImg";
 
 export const MyRecipes = () => {
 
     const [allRecipes, setAllRecipes] = useState([]);
-    const [sortedRecipes, setSortedRecipes] = useState([])
 
     useEffect(() => {
         axios.get('http://localhost:3001/my-recipes')
@@ -13,27 +12,25 @@ export const MyRecipes = () => {
             .catch(err => { console.log(err) })
     }, [])
 
-    useEffect(() => {
-        let sortingRecipes = allRecipes.sort((a, b) => new Date(...b.creationDate.split('/').reverse()) - new Date(...a.creationDate.split('/').reverse()));
-        setSortedRecipes(sortingRecipes)
-        console.log(sortingRecipes)
-    }, allRecipes.length)
 
     return (
         <div className="container fridge-container">
             <h1 className="myRecipesTitle">My recipes  </h1>
             <div className="food-cards d-flex ">
-                {sortedRecipes.map((el, index) => {
+                {allRecipes.map((el, index) => {
                     return (
                         <div className=" card " key={index}>
-                            <div className="card-body" style={{ padding: '10%' }}>
+                            <div className="bin-container d-flex justify-content-end">
+                                <img className="bin-image" src={images[0].binIcon} alt="bin-icon" width="20px" height="20px" />
+                            </div>
+                            <div className="card-body" style={{ padding: '10%' , paddingTop:'28px'}}>
                                 <b>main course</b>
                                 <p>{el.mainCourse.map((item, index) => { return (index === el.mainCourse.length - 1 ? item.name + ';' : item.name + ', ') })}</p>
                                 <b>second course</b>
                                 <p>{el.secondCourse.map((item, index) => { return (index === el.secondCourse.length - 1 ? item.name + ';' : item.name + ', ') })}</p>
                                 <b>side dish</b>
                                 <p>{el.sideDish}</p>
-                                <p className="card-text" style={{ color: 'rgb(163, 163, 163', position: 'absolute', bottom: '20px' }}>{el.creationDate.split('T')[0]}</p>
+                                <p className="card-text card-date" >{el.creationDate.split('T')[0]}</p>
                             </div>
                         </div>
                     )
